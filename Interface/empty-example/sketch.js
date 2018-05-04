8dbb4a1573ad36afd94a2a1391448928161ddfd6
Antonio Vega <a.vegaramirez76@gmail.com>
2018-05-04 13:33:00 +0900

    Update

--- Interface/empty-example/sketch.js
+++ Interface/empty-example/sketch.js
@@ -1,9 +1,30 @@
+// gui params
+var myAngle = 30;
+var myColor = '#eeee00';
+
+var gui;
+
 function setup() {
-  // put setup code here
-  createCanvas(1280,640);
-  background(200);
+
+  createCanvas(windowWidth, windowHeight);
+
+  // Create the GUI
+  sliderRange(0, 90, 1);
+  gui = createGui('p5.gui');
+  gui.addGlobals('myColor', 'myAngle');
+
+  // Only call draw when then gui is changed
+  noLoop();
+
 }
 
+
 function draw() {
-  // put drawing code here
+
+  // this is a piece of cake
+  background(0);
+  fill(myColor);
+  angleMode(DEGREES);
+  arc(width/2, height/2, 100, 100, myAngle/2, 360 - myAngle/2, PIE);
+
 }
