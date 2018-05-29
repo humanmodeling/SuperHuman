ac6b8ed4ede3837bfa90038f42f6277cf3bbe3af
Antonio Vega <a.vegaramirez76@gmail.com>
2018-05-29 21:49:28 +0900

    Scan demo

--- Bluetooth/src/main.cpp
+++ Bluetooth/src/main.cpp
@@ -1,24 +1,44 @@
 #include <Arduino.h>
-#include "BluetoothSerial.h"
+#include "WiFi.h"
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+void setup()
+{
+    Serial.begin(115200);
 
-BluetoothSerial SerialBT;
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
 
-void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("Human"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+    Serial.println("Setup done");
 }
 
-void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(20);
+void loop()
+{
+    Serial.println("scan start");
+
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+            delay(10);
+        }
+    }
+    Serial.println("");
+
+    // Wait a bit before scanning again
+    delay(5000);
 }
